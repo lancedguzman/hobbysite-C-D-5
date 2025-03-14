da4057989001c5fa1af6c0b918ce44b1770c7e0d
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-03-14 19:37:09 +0800

    Merchstore
    I uploaded my application with working url, views, admin, and templates.

---
+++ merchstore/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . views import items_list, items_details, home_page
+
+app_name = 'merchstore'
+
+urlpatterns = [
+    path('', home_page, name='home_page'),
+    path('merchstore/items/', items_list, name='items_list'),
+    path('merchstore/item/<int:item_id>', items_details, name='items_details'),
+]
