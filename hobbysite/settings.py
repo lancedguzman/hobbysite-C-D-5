ef42c9ad741dd6a702d5726a26e291c9d0ba9942
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-05-12 23:04:03 +0800

    Updated settings.py and urls.py
    Updated both to accomodate forum app.

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Manila'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -138,8 +137,4 @@ LOGOUT_REDIRECT_URL = 'merchstore:home_page'
 LOGIN_URL = 'login/'
 
 DEFAULT_FROM_EMAIL = 'webmaster@localhost'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
